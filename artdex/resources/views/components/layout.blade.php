0702b40d4a27c16a0465b76cbde8cde11ad252b3
Jeffrey Bee <jeffreypbee@gmail.com>
2023-11-12 12:20:52 -0500

    Initial frontend layout

---
+++ artdex/resources/views/components/layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Artdex</title>
+</head>
+<body>
+    <header>
+        <h1>Artdex</h1>
+    </header>
+
+    <main>
+        {{$slot}}
+    </main>
+
+    <footer>
+        Copyright &copy; {{date('Y')}} Jeffrey Bee
+    </footer>
+</body>
+</html>
+
+<style>
+
+body {
+    display: grid;
+    height: 100vh;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas: 'header'
+                        'main'
+                        'footer';
+}
+
+header {
+    grid-area: header;
+}
+
+main {
+    grid-area: main;
+}
+
+footer {
+    grid-area: footer;
+}
+
+</style>
