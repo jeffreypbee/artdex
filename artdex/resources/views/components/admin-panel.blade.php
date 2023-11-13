dc14f95d5439c524cfeeb3e2d1ce347dfa04a1db
Jeffrey Bee <jeffreypbee@gmail.com>
2023-11-13 14:13:57 -0500

    Added links to admin nav menu

--- artdex/resources/views/components/admin-panel.blade.php
+++ artdex/resources/views/components/admin-panel.blade.php
@@ -1,6 +1,12 @@
 <div class="admin">
     <div class="admin-nav">
         <a href="/admin">Home</a>
+        <hr>
+        <a href="/admin/art">Art</a>
+        <a href="/admin/art/create">Add New</a>
+        <hr>
+        <a href="/admin/pkmn">Pokemon</a>
+        <a href="/admin/pkmn/create">Add New</a>
     </div>
 
     <div class="admin-main">
@@ -20,6 +26,12 @@
 
 .admin-nav {
     grid-area: admin-nav;
+    display: flex;
+    flex-direction: column;
+}
+
+.admin-nav hr {
+    width: 100%;
 }
 
 .admin-main {
