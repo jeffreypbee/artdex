86221bb6de8da343bed5f9988cc14f56cc62e8e6
Jeffrey Bee <jeffreypbee@gmail.com>
2023-11-13 11:43:27 -0500

    Created separate stylesheet file

--- artdex/resources/views/components/layout.blade.php
+++ artdex/resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/style.css">
     <title>Artdex</title>
 </head>
 <body>
@@ -21,61 +22,3 @@
     </footer>
 </body>
 </html>
-
-<style>
-
-:root {
-    --darkergray: #111;
-    --darkgray: #222;
-    --gray: #333;
-    --white: #eee;
-    --lightblue: #66aaff;
-}
-
-body {
-    background: linear-gradient(var(--darkgray), var(--darkergray));
-    color: var(--white);
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    margin: 0;
-    padding: 0;
-    display: grid;
-    height: 100vh;
-    grid-template-rows: auto 1fr auto;
-    grid-template-areas: 'header'
-                        'main'
-                        'footer';
-}
-
-header {
-    grid-area: header;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border-bottom: 1px solid var(--gray);
-}
-
-header h1 {
-    margin: 0;
-    padding: 0;
-}
-
-main {
-    grid-area: main;
-    display: flex;
-    justify-content: center;
-    padding: 10px;
-}
-
-footer {
-    grid-area: footer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-a {
-    color: var(--lightblue);
-    text-decoration: none;
-}
-
-</style>
