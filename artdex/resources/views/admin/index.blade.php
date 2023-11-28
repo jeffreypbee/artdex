391d928b9941e59fb9b59b07685b0f39df312ea5
Jeffrey Bee <jeffreypbee@gmail.com>
2023-11-27 19:31:58 -0500

    Stylin

--- artdex/resources/views/admin/index.blade.php
+++ artdex/resources/views/admin/index.blade.php
@@ -2,39 +2,42 @@
 
 <x-admin-panel>
     
-    <section class="add-panel">
-        <x-widget title="Add New Stuff">
-            <div class="art">
-                <a href="/admin/art/create"><button>Add Art</button></a>
-            </div>
+    <div class="columns">
+        <section>
+            <x-widget title="Add New Stuff">
+                <div class="art">
+                    <a href="/admin/art/create"><button>Add Art</button></a>
+                </div>
+            
+                <div class="pkmn">
+                    <a href="/admin/pkmn/create"><button>Add Pokemon</button></a>
+                </div>
         
-            <div class="pkmn">
-                <a href="/admin/pkmn/create"><button>Add Pokemon</button></a>
-            </div>
+                <div class="gen">
+                    <a href=""><button>Add Generation</button></a>
+                </div>
+            </x-widget>
+        </section>
     
-            <div class="gen">
-                <a href=""><button>Add Generation</button></a>
-            </div>
-        </x-widget>
-    </section>
+        <section>
+            <x-widget title="Upcoming Art">
+                @foreach ($upcoming as $art)
+                    <x-dex-card :pkmn="$art->pokemon" />
+                @endforeach
+            </x-widget>
+        </section>
+    
+        <section>
+            <x-widget title="Unscheduled Art">
+                @foreach ($unscheduled as $art)
+                    <x-dex-card :pkmn="$art->pokemon" />
+                @endforeach
+            </x-widget>
+        </section>
+    </div>
+    
 
-    <h2>Upcoming Art</h2>
-    <section>
-        <div class="dex-card-container">
-        @foreach ($upcoming as $art)
-            <x-dex-card :pkmn="$art->pokemon" />
-        @endforeach
-        </div>    
-    </section>
-
-    <h2>Unscheduled Art</h2>
-    <section>
-        <div class="dex-card-container">
-            @foreach ($unscheduled as $art)
-                <x-dex-card :pkmn="$art->pokemon" />
-            @endforeach
-            </div> 
-    </section>
+    
 </x-admin-panel>
 
 
@@ -42,35 +45,6 @@
 
 <style>
 
-.add-panel {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-areas: 'art pkmn gen';
-    gap: 10px;
-    width: 100%;
-}
 
-.add-panel .art {
-    grid-area: art;
-}
-
-.add-panel .pkmn {
-    grid-area: pkmn;
-}
-
-.admin-panel .gen {
-    grid-area: gen;
-}
-
-.add-panel .pkmn, .add-panel .art, .add-panel .gen {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.add-panel .art button, .add-panel .pkmn button, .add-panel .gen button {
-    height: 100px;
-    width: 100px;
-}
 
 </style>
