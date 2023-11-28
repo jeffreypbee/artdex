cc33e58ec77028b8ec76d96ce8c6006a942f02c3
Jeffrey Bee <jeffreypbee@gmail.com>
2023-11-28 18:49:44 -0500

    Styling

--- artdex/resources/views/components/widget.blade.php
+++ artdex/resources/views/components/widget.blade.php
@@ -15,16 +15,22 @@
     flex-direction: column;
     border-radius: 20px;
     padding: 10px;
-    background: var(--gray);
+    width: 100%;
+    background: linear-gradient(var(--darkgray), transparent);
 }
 
 .widget .title {
+    font-size: 1.5rem;
     font-weight: bold;
     border-bottom: 1px solid white;
 }
 
 .widget .content {
     padding: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
 }
 
 </style>
--- artdex/resources/views/dex/stats.blade.php
+++ artdex/resources/views/dex/stats.blade.php
@@ -4,38 +4,39 @@
     <x-progress-widget text="Pokemon Drawn" :number="count($art)" :total="count($dex)" color="#ffffff" icon="pencil" />
 
     <h2>Breakdowns</h2>
-    <div class="stats-columns">
-        <section class="type-progress-bars">
-            <h3>Pokemon Types</h3>
-            @foreach ($types as $type)
-            @php
-                $artCount = 0;
-                foreach($type->pokemon as $pkmn) {
-                    if ($pkmn->art !== null) {
-                        $artCount++;
+    <div class="columns">
+        <section>
+            <x-widget title="Pokemon Types">
+                @foreach ($types as $type)
+                @php
+                    $artCount = 0;
+                    foreach($type->pokemon as $pkmn) {
+                        if ($pkmn->art !== null) {
+                            $artCount++;
+                        }
                     }
-                }
-            @endphp
-            <div>
-                <x-progress-widget :text="ucfirst($type->name) . ' Pokemon Drawn'" :number="$artCount" :total="count($type->pokemon)" :color="$type->color1" :icon="$type->icon" />
-            </div>        
-            @endforeach
+                @endphp
+                <div>
+                    <x-progress-widget :text="ucfirst($type->name) . ' Pokemon Drawn'" :number="$artCount" :total="count($type->pokemon)" :color="$type->color1" :icon="$type->icon" />
+                </div>        
+                @endforeach
+            </x-widget>
         </section>
 
-        <section class="generation-progress-bars">
-            <h3>Generations</h3>
-            @foreach ($gens as $gen)
-            @php
-                $artCount = 0;
-                foreach($gen->pokemon as $pkmn) {
-                    if ($pkmn->art !== null) {
-                        $artCount++;
+        <section>
+            <x-widget title="Generations">
+                @foreach ($gens as $gen)
+                @php
+                    $artCount = 0;
+                    foreach($gen->pokemon as $pkmn) {
+                        if ($pkmn->art !== null) {
+                            $artCount++;
+                        }
                     }
-                }
-            @endphp
-            <x-progress-widget text="Generation {{$gen->id}} Pokemon Drawn" :number="$artCount" :total="count($gen->pokemon)" :color="$gen->color" :icon="$gen->id" />
-            @endforeach
-            
+                @endphp
+                <x-progress-widget text="Generation {{$gen->id}} Pokemon Drawn" :number="$artCount" :total="count($gen->pokemon)" :color="$gen->color" :icon="$gen->id" />
+                @endforeach
+            </x-widget>
         </section>
     </div>
     
