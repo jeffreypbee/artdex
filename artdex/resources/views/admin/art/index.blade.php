685c7ffa7f3618bfa4486424059fac1722cf22ae
Jeffrey Bee <jeffreypbee@gmail.com>
2023-12-12 13:06:16 -0500

    Styling admin tables

--- artdex/resources/views/admin/art/index.blade.php
+++ artdex/resources/views/admin/art/index.blade.php
@@ -1,10 +1,23 @@
 <x-layout>
     <x-admin-panel>
         <h2>Art</h2>
-        @foreach ($art as $piece)
-            <div>
-                {{$piece->pokemon->name}}
-            </div>
+        <table class="table">
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Form</th>
+                <th>Date</th>
+                <th>Admin</th>
+            </tr>
+            @foreach ($art as $piece)
+            <tr>
+                <td>#{{$piece->pokemon->formattedNumber()}}</td>
+                <td>{{$piece->pokemon->name}}</td>
+                <td>{{$piece->pokemon->form}}</td>
+                <td>{{$piece->date}}</td>
+                <td><button>Edit</button></td>
+            </tr>
         @endforeach
+        </table>        
     </x-admin-panel>
 </x-layout>
