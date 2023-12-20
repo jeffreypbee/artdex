32bcf7faeb5c5ca8123f7444ea8d020c466b9f42
Jeffrey Bee <jeffreypbee@gmail.com>
2023-12-20 13:16:22 -0500

    Styling

--- artdex/resources/views/components/admin-panel.blade.php
+++ artdex/resources/views/components/admin-panel.blade.php
@@ -1,14 +1,17 @@
 <div class="admin">
     <div class="admin-nav">
+        <h3>Admin Panel</h3>
         <section>
             <a href="/admin">Home</a>
         </section>
         <section>
-            <a href="/admin/art">Art</a>
+            <h4>Art</h4>
+            <a href="/admin/art">All</a>
             <a href="/admin/art/create">Add New</a>
         </section>
         <section>
-            <a href="/admin/pkmn">Pokemon</a>
+            <h4>Pokemon</h4>
+            <a href="/admin/pkmn">All</a>
             <a href="/admin/pkmn/create">Add New</a>
         </section>        
     </div>
