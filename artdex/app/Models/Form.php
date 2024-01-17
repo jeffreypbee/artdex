f0aadf0ade951358ca1ae731d686ea2488ae4753
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-17 10:47:31 -0500

    Added Pokemon and Form relationship

--- artdex/app/Models/Form.php
+++ artdex/app/Models/Form.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Form extends Model
 {
     use HasFactory;
+
+    public function Pokemon(): BelongsTo {
+        return $this->belongsTo(Pokemon::class);
+    }
 }
--- artdex/app/Models/Pokemon.php
+++ artdex/app/Models/Pokemon.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pokemon extends Model
 {
     use HasFactory;
+
+    public function forms(): HasMany {
+        return $this->hasMany(Form::class);
+    }
 }
