eb9fcf7109963353518372b9c72dde14d06d86f0
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-17 11:48:22 -0500

    Created Pokemon Generation relationship

--- artdex/app/Models/Pokemon.php
+++ artdex/app/Models/Pokemon.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Pokemon extends Model
 {
     use HasFactory;
+
+    public function generation(): BelongsTo {
+        return $this->belongsTo(Generation::class);
+    }
 
     public function forms(): HasMany {
         return $this->hasMany(Form::class);
