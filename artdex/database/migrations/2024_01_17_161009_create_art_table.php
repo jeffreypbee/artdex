d93cba5c349a2a0de747f125f071b33e1009b464
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-17 11:11:30 -0500

    Created Art Model

---
+++ artdex/database/migrations/2024_01_17_161009_create_art_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('art', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pokemon_id')->constrained('pokemon')->onDelete('cascade');
+            $table->date('publish_date')->nullable();
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('art');
+    }
+};
