c14b4b789fef94ddcc062f7b543d7e572a4a38a3
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-17 12:20:17 -0500

    Update DatabaseSeeder.php

--- artdex/database/seeders/DatabaseSeeder.php
+++ artdex/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory()->create([
+            'name' => 'admin',
+            'password' => Hash::make('password')
+        ]);
 
         $generationSeeder = new GenerationSeeder();
         $generationSeeder->run();
