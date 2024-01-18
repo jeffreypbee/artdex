3b81094586379c25ca5cab35afdcf4c91c87961e
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-18 14:20:04 -0500

    Created Filament Resources

---
+++ artdex/app/Filament/Resources/PokemonResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PokemonResource\Pages;
+use App\Filament\Resources\PokemonResource\RelationManagers;
+use App\Models\Pokemon;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PokemonResource extends Resource
+{
+    protected static ?string $model = Pokemon::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPokemon::route('/'),
+            'create' => Pages\CreatePokemon::route('/create'),
+            'edit' => Pages\EditPokemon::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ artdex/app/Filament/Resources/PokemonResource/Pages/ListPokemon.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\PokemonResource\Pages;
+
+use App\Filament\Resources\PokemonResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListPokemon extends ListRecords
+{
+    protected static string $resource = PokemonResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
