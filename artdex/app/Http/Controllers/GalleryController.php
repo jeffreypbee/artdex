824cb739b41993eda17930d586a1113f70ecfd94
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-20 15:34:55 -0500

    Update GalleryController.php

--- artdex/app/Http/Controllers/GalleryController.php
+++ artdex/app/Http/Controllers/GalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Art;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
@@ -11,6 +12,8 @@ class GalleryController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        return view('gallery', [
+            'art' => Art::orderBy('publish_date', 'DESC')->get()
+        ]);
     }
 }
