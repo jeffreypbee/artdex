f0fd1bc72d64b772f4d00d7172967076463ed45a
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-20 15:46:20 -0500

    Updated dex page

--- artdex/resources/views/home.blade.php
+++ artdex/resources/views/home.blade.php
@@ -1,12 +1,3 @@
 <x-app-layout>
-    <div class="flex flex-wrap gap-10">
-        @foreach ($pokemon as $pkmn)
-            <div class="w-50 bg-white">
-                {{$pkmn->name}}
-                @unless (count($pkmn->art) === 0)
-                    <img src="/storage/{{$pkmn->art[0]->image}}" alt="" style="width: 100px">
-                @endunless
-            </div>
-        @endforeach
-    </div>
+
 </x-app-layout>
