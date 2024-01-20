5cb95eaafbe86f064b17574dbb2dd24142340bf0
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-20 17:48:25 -0500

    Styling

--- artdex/resources/views/layouts/app.blade.php
+++ artdex/resources/views/layouts/app.blade.php
@@ -14,8 +14,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans antialiased bg-slate-900 text-white">
+        <div class="min-h-screen">
             {{-- <livewire:layout.navigation /> --}}
 
             {{-- <!-- Page Heading -->
