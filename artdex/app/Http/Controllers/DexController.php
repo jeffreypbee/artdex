76f1a0966257090314e57277b69c659b300f6f03
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-20 15:34:29 -0500

    Update DexController.php

--- artdex/app/Http/Controllers/DexController.php
+++ artdex/app/Http/Controllers/DexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Art;
 use Illuminate\Http\Request;
 
 class DexController extends Controller
@@ -11,6 +12,8 @@ class DexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        return view('gallery', [
+            'art' => Art::orderBy('publish_date', 'DESC')->get()
+        ]);
     }
 }
