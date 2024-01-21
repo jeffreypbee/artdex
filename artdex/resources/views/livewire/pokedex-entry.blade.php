8c1dc717cdda1d5e4efd2556d88a6ede0b738f5a
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-21 16:45:21 -0500

    Update pokedex-entry.blade.php

--- artdex/resources/views/livewire/pokedex-entry.blade.php
+++ artdex/resources/views/livewire/pokedex-entry.blade.php
@@ -2,11 +2,12 @@
     <div class="h-1/4 rounded-t-lg border-b-2 border-white" style="background: {{$background}}">
 
     </div>
-    <div class="absolute top-1/2 -left-5 transform -translate-y-1/2">
+    <div>
         @if ($image == null)
-            <i class="fa-solid fa-question fa-2xl absolute left-12 text-gray-500"></i>
+            <img src="/images/pokeball_gray.png" alt="" class="absolute top-1/2 transform -translate-y-1/2 opacity-50">
+            {{-- <i class="fa-solid fa-question fa-2xl absolute left-12 text-gray-500"></i> --}}
         @else
-            <img src="/storage/{{$image}}" alt="" style="width: 150px">
+            <img src="/storage/{{$image}}" alt="" style="width: 150px" class="absolute top-1/2 -left-5 transform -translate-y-1/2">
         @endif
     </div>
     <div class="h-3/4 flex flex-col items-end justify-around pb-1 pr-2 leading-none">
