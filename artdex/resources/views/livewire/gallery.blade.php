56b163cd2710862301fd095f8f32fdb566afd6f0
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-21 13:56:24 -0500

    Update gallery.blade.php

--- artdex/resources/views/livewire/gallery.blade.php
+++ artdex/resources/views/livewire/gallery.blade.php
@@ -1,7 +1,7 @@
 <div class="flex">
     @foreach ($gallery as $art)
         <div>
-            <img src="/storage/{{$art->image}}" alt="" style="width: 100px">
+            <img src="/storage/{{$art->image}}" alt="" style="width: 250px">
         </div>
     @endforeach
 </div>
