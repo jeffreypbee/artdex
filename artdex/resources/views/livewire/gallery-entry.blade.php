17faea89f8e16543e3ed268895c6efa6811bcf07
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 10:21:54 -0500

    Update gallery-entry.blade.php

--- artdex/resources/views/livewire/gallery-entry.blade.php
+++ artdex/resources/views/livewire/gallery-entry.blade.php
@@ -1,7 +1,14 @@
 <div class="relative">
     <img src="/storage/{{$art->image}}" alt="" class="hover:cursor-pointer" style="width: 250px">
-    <div class="absolute bottom left-1/2 transform -translate-x-1/2 backdrop-blur-sm rounded-md">
-        #{{$number}}
-        {{$title}}
+    <div class="p-2 flex flex-col absolute bottom-0 left-0 w-full backdrop-blur-sm rounded-md">
+        <div>
+            #{{$number}}
+        </div>
+        <div>
+            {{$title}}
+        </div>
+        <div>
+            {{date('M d, Y', strtotime($art->publish_date))}}
+        </div>
     </div>
 </div>
