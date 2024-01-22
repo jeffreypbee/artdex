5d722be1f77276357400f093af8565b286de1393
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 11:34:51 -0500

    Update gallery-entry.blade.php

--- artdex/resources/views/livewire/gallery-entry.blade.php
+++ artdex/resources/views/livewire/gallery-entry.blade.php
@@ -1,6 +1,11 @@
-<div class="relative">
-    <img src="/storage/{{$art->image}}" alt="" class="hover:cursor-pointer" style="width: 250px">
-    <div class="p-2 flex flex-col absolute bottom-0 left-0 w-full backdrop-blur-sm rounded-md">
+<div x-data="{showInfo: false}"
+    @mouseenter="showInfo = true"
+    @mouseleave="showInfo = false"
+    class="relative hover:cursor-pointer hover:bg-slate-600 rounded-lg">
+    <img src="/storage/{{$art->image}}" alt="" class="" style="width: 250px">
+    <div
+        x-show="showInfo"
+        class="p-2 flex flex-col absolute bottom-0 left-0 w-full backdrop-blur-sm rounded-lg">
         <div>
             #{{$number}}
         </div>
