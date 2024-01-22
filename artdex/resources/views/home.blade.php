eeabb0b3cf743e1b07028a494fe7fa37dd9a845e
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 13:16:12 -0500

    Added bounce

--- artdex/resources/views/home.blade.php
+++ artdex/resources/views/home.blade.php
@@ -4,7 +4,7 @@
     Latest Art
     <div class="flex">
         @foreach ($latestArt as $art)
-            <img src="/storage/{{$art->image}}" alt="" style="width: 100px">
+            <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce" style="width: 100px">
         @endforeach
     </div>
 </x-app-layout>
--- artdex/resources/views/livewire/pokedex-entry.blade.php
+++ artdex/resources/views/livewire/pokedex-entry.blade.php
@@ -1,4 +1,8 @@
-<div class="rounded-lg relative" style="height: 120px; width: 200px; background: rgb(40, 52, 90)">
+<div x-data="{hovering: false}"
+    @mouseenter="hovering=true"
+    @mouseleave="hovering=false"
+    class="rounded-lg relative hover:cursor-pointer"
+    style="height: 120px; width: 200px; background: rgb(40, 52, 90)">
     <div class="h-1/4 rounded-t-lg border-b-2 border-white" style="background: {{$background}}">
 
     </div>
@@ -7,7 +11,7 @@
             <img src="/images/pokeball_gray.png" alt="" class="absolute top-1/2 transform -translate-y-1/2 opacity-50">
             {{-- <i class="fa-solid fa-question fa-2xl absolute left-12 text-gray-500"></i> --}}
         @else
-            <img src="/storage/{{$image}}" alt="" style="width: 150px" class="absolute top-1/2 -left-5 transform -translate-y-1/2">
+            <img src="/storage/{{$image}}" alt="" style="width: 150px" class="absolute -bottom-2 -left-5 transition ease-in-out" :class="hovering ? 'animate-bounce' : ''">
         @endif
     </div>
     <div class="h-3/4 flex flex-col items-end justify-around pb-1 pr-2 leading-none">
