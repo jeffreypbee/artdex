0c1fe965f8d40728fc9b1656c9db2a82a435bd7e
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 12:19:15 -0500

    Updated stats page

--- artdex/app/Http/Controllers/StatsController.php
+++ artdex/app/Http/Controllers/StatsController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Type;
+use App\Models\Generation;
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
     public function __invoke(Request $request) {
-        return view('stats');
+        return view('stats', [
+            'types' => Type::orderBy('name', 'ASC')->get(),
+            'generations' => Generation::orderBy('generation', 'ASC')->get()
+        ]);
     }
 }
--- artdex/resources/views/stats.blade.php
+++ artdex/resources/views/stats.blade.php
@@ -1,3 +1,25 @@
 <x-app-layout>
     <x-page-title>Stats</x-page-title>
+
+    @foreach ($types as $type)
+        @php
+            $artCount = 0;
+            foreach($type->getPokemon() as $pokemon) {
+                if ($pokemon->hasArt()) {
+                    $artCount++;
+                }
+            }
+            $fraction = $artCount / count($type->getPokemon())
+        @endphp
+        <div class="flex items-center gap-2">
+            <div class="rounded h-2 bg-black"
+                style="width: 200px">
+                <div class="rounded h-2"
+                    style="width: {{floor(200 * $fraction)}}px; background: {{$type->color}}"></div>
+            </div>
+            <div>
+                {{floor($fraction * 100)}}% Pokemon Drawn
+            </div>
+        </div>
+    @endforeach
 </x-app-layout>
