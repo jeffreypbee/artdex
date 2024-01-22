3e11ab0d500af7dba6b044c942e4f7aeefd4e575
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 15:17:45 -0500

    Update completion-bar.blade.php

--- artdex/resources/views/components/completion-bar.blade.php
+++ artdex/resources/views/components/completion-bar.blade.php
@@ -1,13 +1,14 @@
 @props(['title', 'icon', 'color', 'fraction'])
 
-<div class="flex items-center gap-2">
+<div class="flex justify-between items-center gap-2">
     <i class="fa-solid {{$icon ?? 'fa-circle'}}" style="color: {{$color ?? 'white'}}"></i>
+    {{$title ?? ''}}
     <div class="rounded h-2 bg-black"
         style="width: 200px">
         <div class="rounded h-2"
             style="width: {{floor(200 * $fraction)}}px; background: {{$color ?? 'white'}}"></div>
     </div>
     <div>
-        {{floor($fraction * 100)}}% Pokemon Drawn
+        {{floor($fraction * 100)}}%
     </div>
 </div>
