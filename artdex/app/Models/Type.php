2ff69724d01f6a08b5a655156a76c7811db81134
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 12:19:01 -0500

    Created logic to get pokemon by type

--- artdex/app/Models/Type.php
+++ artdex/app/Models/Type.php
@@ -2,19 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Type extends Model
 {
     use HasFactory;
 
-    public function pokemon(): MorphToMany {
-        return $this->morphedByMany(Pokemon::class, 'typeable');
+    public function type1Pokemon(): HasMany {
+        return $this->hasMany(Pokemon::class, 'type1_id');
     }
 
-    public function forms(): MorphToMany {
-        return $this->morphedByMany(PokemonForm::class, 'typeable');
+    public function type2Pokemon(): HasMany {
+        return $this->hasMany(Pokemon::class, 'type2_id');
     }
+
+    protected function pokemonQuery() {
+        return Pokemon::where('type1_id', '=', $this->id)->orWhere('type2_id', '=', $this->id);
+    }
+
+    public function getPokemon() {
+        return $this->pokemonQuery()->get();
+    }
+
 }
