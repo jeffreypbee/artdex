1263b23932ee7f3439962e49777198aaa8f7e17f
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 11:59:49 -0500

    Added buttons for future filtering functionality

--- artdex/app/Livewire/Pokedex.php
+++ artdex/app/Livewire/Pokedex.php
@@ -2,15 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Type;
 use App\Models\Pokemon;
 use Livewire\Component;
+use App\Models\Generation;
 
 class Pokedex extends Component
 {
     public function render()
     {
         return view('livewire.pokedex', [
-            'pokedex' => Pokemon::orderBy('number', 'ASC')->get()
+            'pokedex' => Pokemon::orderBy('number', 'ASC')->get(),
+            'generations' => Generation::orderBy('generation', 'ASC')->get(),
+            'types' => Type::orderBy('name', 'ASC')->get()
         ]);
     }
 }
--- artdex/resources/views/livewire/pokedex.blade.php
+++ artdex/resources/views/livewire/pokedex.blade.php
@@ -1,4 +1,22 @@
-<div>
+<div class="w-full flex flex-col items-center">
+
+    <div class="m-b-5 w-full flex flex-col items-center">
+        Generations
+        <div class="flex">
+            @foreach ($generations as $generation)
+                <button>{{$generation->generation}}</button>
+            @endforeach
+        </div>
+
+        Types
+        <div class="flex">
+            @foreach ($types as $type)
+                <button><i class="fa-solid {{$type->icon}}"></i></button>
+            @endforeach
+        </div>
+    </div>
+
+
     <div class="flex flex-wrap gap-5">
         @foreach ($pokedex as $pokemon)
             <livewire:pokedex-entry :pokemon="$pokemon" />
