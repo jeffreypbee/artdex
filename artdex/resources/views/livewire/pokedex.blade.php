72ee2085b820aefdb764670cfa02cc52289ca130
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-23 16:03:44 -0500

    Update pokedex.blade.php

--- artdex/resources/views/livewire/pokedex.blade.php
+++ artdex/resources/views/livewire/pokedex.blade.php
@@ -8,7 +8,7 @@
                 <button
                     wire:key='gen-{{$generation->id}}'
                     wire:click='filterGen({{$generation->generation}})'
-                    class="m-1 w-8 h-8 rounded-2xl flex justify-center items-center bg-blue-gray border-2 border-transparent hover:border-gray-600 {{$this->gen == $generation->generation ? 'border-white' : ''}}"
+                    class="m-1 w-8 h-8 rounded-2xl flex justify-center items-center bg-blue-gray border-2 border-transparent hover:border-gray-600 hover:animate-pulse {{$this->gen == $generation->generation ? 'border-white' : ''}}"
                     >
                     <i class="fa-solid {{$generation->icon}} fa-fw" style="color: {{$generation->color}}"></i>
                 </button>
@@ -21,7 +21,7 @@
                 <button
                     wire:key='type-{{$type->id}}'
                     wire:click='filterType("{{$type->name}}")'
-                    class="m-1 w-8 h-8 rounded-2xl flex justify-center items-center bg-blue-gray border-2 border-transparent hover:border-gray-600 {{$this->type == $type->name ? 'border-white' : ''}}"
+                    class="m-1 w-8 h-8 rounded-2xl flex justify-center items-center bg-blue-gray border-2 border-transparent hover:border-gray-600 hover:animate-pulse {{$this->type == $type->name ? 'border-white' : ''}}"
                     >
                     <i class="fa-solid {{$type->icon}} fa-fw" style="color: {{$type->color}}"></i>
                 </button>
@@ -29,7 +29,7 @@
         </div>
 
         <button
-            class="m-2 p-2 leading-none rounded-2xl flex justify-center items-center bg-blue-gray border-2 border-transparent hover:border-gray-600"
+            class="m-2 p-2 leading-none rounded-2xl flex justify-center items-center bg-blue-gray border-2 border-transparent hover:border-gray-600 hover:animate-pulse"
             wire:click='resetFilters'>
                 Reset Filters
             </button>
