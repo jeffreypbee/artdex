82e75d37b97536358b6c995d0cd4e80146dba618
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 21:21:13 -0500

    Update nav-link.blade.php

--- artdex/resources/views/components/nav-link.blade.php
+++ artdex/resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'p-2 flex justify-between items-end leading-none gap-1 border-r-2 border-indigo-400 text-sm font-medium focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'p-2 flex justify-between items-end leading-none gap-1 border-r-2 border-transparent text-sm font-medium hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'p-2 flex justify-between items-end leading-none gap-1 border-r-2 border-white text-sm font-medium focus:outline-none focus:border-white transition duration-150 ease-in-out'
+            : 'p-2 flex justify-between items-end leading-none gap-1 border-r-2 border-transparent text-sm font-medium hover:border-gray-400 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
