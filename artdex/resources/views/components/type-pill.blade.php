69867ba3bf8a613a0c9925cfd0d40bb8bf6f9877
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-23 11:49:24 -0500

    Update type-pill.blade.php

--- artdex/resources/views/components/type-pill.blade.php
+++ artdex/resources/views/components/type-pill.blade.php
@@ -1,6 +1,7 @@
 @props(['type'])
 
-<div class="flex justify-between items-center p-1 rounded-lg text-indigo-900" style="width: 100px; background: {{$type->color}}">
+<div class="flex justify-between items-center p-1 rounded-lg font-extrabold"
+    style="width: 100px; background: linear-gradient(135deg, {{$type->color}} 40%, rgb(40, 52, 90) 40%)">
     <i class="fa-solid {{$type->icon}} fa-fw"></i>
-    {{$type->name}}
+    <div class="pr-1">{{$type->name}}</div>
 </div>
