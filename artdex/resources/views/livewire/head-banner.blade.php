766be8e02a10824255083f8ea72adf1e6dbcf20b
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-22 21:32:52 -0500

    Update head-banner.blade.php

--- artdex/resources/views/livewire/head-banner.blade.php
+++ artdex/resources/views/livewire/head-banner.blade.php
@@ -1,8 +1,8 @@
-<div class="border-b-2 border-white"
+<div x-data="{hovering: false}" class="border-b-2 border-white transition-all ease-in-out" :class="hovering ? 'animate-pulse' : ''"
     style="background: center/150% no-repeat url('/images/banner.png')">
     <div class="p-5 flex justify-between backdrop-blur-md"
-        style="background: linear-gradient(to right, black, transparent, black)">
-        <div class="text-2xl font-extrabold">
+        style="background: linear-gradient(to right, rgb(15 23 42), transparent, rgb(15 23 42))">
+        <div class="text-2xl font-extrabold" @mouseenter="hovering=true" @mouseleave="hovering=false">
             <a href="/">{{config('app.name')}}</a>
         </div>
         <livewire:completion-widget />
