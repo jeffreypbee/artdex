b77254922bbd2ec7f120b5387c0da94d4677dfef
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-25 10:13:18 -0500

    Update home.blade.php

--- artdex/resources/views/home.blade.php
+++ artdex/resources/views/home.blade.php
@@ -1,18 +1,33 @@
 <x-app-layout>
 
-
-    Latest Art
-    <div class="flex">
+    <div class="font-bold text-lg">
+        Latest Art
+    </div>
+    <div class="flex m-5">
         @foreach ($latestArt as $art)
-            <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce" style="width: 100px">
+            <div class="flex flex-col items-center">
+                <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce" style="width: 100px">
+                <div class="text-sm">
+                    {{date('M d, Y', strtotime($art->publish_date))}}
+                </div>
+            </div>
+
         @endforeach
     </div>
 
-    Upcoming Art
-    <div class="flex">
+    <div class="font-bold text-lg">
+        Upcoming Art
+    </div>
+    <div class="flex m-5">
         @foreach ($upcomingArt as $art)
-            <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce"
-            style="width: 100px; filter: brightness(0) invert(1)">
+            <div class="flex flex-col items-center">
+                <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce"
+                style="width: 100px; filter: brightness(0) invert(1)">
+                <div class="text-sm">
+                    {{date('M d, Y', strtotime($art->publish_date))}}
+                </div>
+            </div>
+
         @endforeach
     </div>
 
