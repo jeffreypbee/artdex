5e4dfdf95ad810f8450966b607e6115c4539b792
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-26 11:22:15 -0500

    Update home.blade.php

--- artdex/resources/views/home.blade.php
+++ artdex/resources/views/home.blade.php
@@ -1,34 +1,47 @@
 <x-app-layout>
 
-    <div class="font-bold text-lg">
-        Latest Art
-    </div>
-    <div class="flex m-5">
-        @foreach ($latestArt as $art)
-            <div class="flex flex-col items-center">
-                <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce" style="width: 100px">
-                <div class="text-sm">
-                    {{date('M d, Y', strtotime($art->publish_date))}}
+    <section class="m-5 flex flex-col items-center">
+        <x-page-title>
+            Latest Art
+        </x-page-title>
+        <div class="flex m-5 gap-2">
+            @foreach ($latestArt as $art)
+                @php
+                    $colors = $art->artable->getColors();
+                    $background = 'white';
+                    if (count($colors) === 1) {
+                        $background = $colors[0];
+                    } else {
+                        $background = 'linear-gradient(to right, ' . implode(', ', $colors) . ')';
+                    }
+                @endphp
+                <div class="flex flex-col items-center rounded-xl"
+                    style="background: {{$background}};">
+                    <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce"
+                        style="width: 100px;">
+                    <div class="w-full p-2 align-middle text-sm border-t-2 border-white rounded-b-lg bg-blue-gray">
+                        {{date('M d, Y', strtotime($art->publish_date))}}
+                    </div>
                 </div>
+
+            @endforeach
+        </div>
+    </section>
+
+
+    @unless (count($upcomingArt) === 0)
+        <section class="m-5 flex flex-col items-center">
+            <x-page-title>
+                Who's that Pok&eacute;mon?
+            </x-page-title>
+            <img src="/storage/{{$upcomingArt[0]->image}}" alt="" class="-m-5" style="filter: brightness(0) invert(1)">
+            Find Out
+            <div class="font-bold">
+                {{Carbon\Carbon::parse($upcomingArt[0]->publish_date)->diffForHumans()}}
             </div>
+        </section>
+    @endunless
 
-        @endforeach
-    </div>
 
-    <div class="font-bold text-lg">
-        Upcoming Art
-    </div>
-    <div class="flex m-5">
-        @foreach ($upcomingArt as $art)
-            <div class="flex flex-col items-center">
-                <img src="/storage/{{$art->image}}" alt="" class="transition ease-in-out hover:animate-bounce"
-                style="width: 100px; filter: brightness(0) invert(1)">
-                <div class="text-sm">
-                    {{date('M d, Y', strtotime($art->publish_date))}}
-                </div>
-            </div>
-
-        @endforeach
-    </div>
 
 </x-app-layout>
