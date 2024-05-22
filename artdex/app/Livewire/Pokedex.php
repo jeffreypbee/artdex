466f8e1ab6d1404fc5e9d5bfb229d0d26aa945cb
Jeffrey Bee <jeffreypbee@gmail.com>
2024-05-22 09:01:59 -0400

    Converted pokedex to a Livewire component

--- artdex/app/Livewire/Pokedex.php
+++ artdex/app/Livewire/Pokedex.php
@@ -3,11 +3,22 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Statamic\Facades\Entry;
 
 class Pokedex extends Component
 {
+    public function filterPokedex() {
+        $pokedex = Entry::query()
+            ->where('collection', 'pokemon')
+            ->orderBy('number')
+            ->get();
+        return $pokedex;
+    }
+
     public function render()
     {
-        return view('livewire.pokedex');
+        return view('livewire.pokedex', [
+            'pokedex' => $this->filterPokedex()
+        ]);
     }
 }
