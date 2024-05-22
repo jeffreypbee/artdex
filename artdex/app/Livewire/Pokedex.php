8fc8c8b214f4bceef61025b9996245098532873a
Jeffrey Bee <jeffreypbee@gmail.com>
2024-05-22 08:10:07 -0400

    Created Pokedex Livewire component

---
+++ artdex/app/Livewire/Pokedex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Pokedex extends Component
+{
+    public function render()
+    {
+        return view('livewire.pokedex');
+    }
+}
