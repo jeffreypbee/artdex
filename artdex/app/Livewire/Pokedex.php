c90c5bc59f22f5bb868fd91a623100794108b155
Jeffrey Bee <jeffreypbee@gmail.com>
2024-05-22 14:34:00 -0400

    Revert "Converted pokedex to a Livewire component"
    This reverts commit 466f8e1ab6d1404fc5e9d5bfb229d0d26aa945cb.

--- artdex/app/Livewire/Pokedex.php
+++ artdex/app/Livewire/Pokedex.php
@@ -3,22 +3,11 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use Statamic\Facades\Entry;
 
 class Pokedex extends Component
 {
-    public function filterPokedex() {
-        $pokedex = Entry::query()
-            ->where('collection', 'pokemon')
-            ->orderBy('number')
-            ->get();
-        return $pokedex;
-    }
-
     public function render()
     {
-        return view('livewire.pokedex', [
-            'pokedex' => $this->filterPokedex()
-        ]);
+        return view('livewire.pokedex');
     }
 }
