28e70b7ef2d97c1329846ee82234888c10fe9919
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-02 14:30:57 -0400

    Added filter section

--- artdex/resources/js/components/Pokedex.vue
+++ artdex/resources/js/components/Pokedex.vue
@@ -1,13 +1,23 @@
 <template>
-    <div v-if="loadingPokedex">
-        Loading
-    </div>
-    <div v-else>
-        <div class="flex flex-wrap justify-center">
-            <PokemonCard v-for="pokemon in pokedex" :key="pokemon.id" :pokemon="pokemon" />
+    <div class="bg-gray">
+        <div v-if="loadingFilters">Loading</div>
+        <div v-else class="flex">
+            <div v-for="type in types" :key="type.id">
+                <i class="fa-solid p-1" :class="type.icon" :style="{ color: type.color }"></i>
+            </div>
         </div>
-
     </div>
+    <div>
+        <div v-if="loadingPokedex">
+            Loading
+        </div>
+        <div v-else>
+            <div class="flex flex-wrap justify-center">
+                <PokemonCard v-for="pokemon in pokedex" :key="pokemon.id" :pokemon="pokemon" />
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -20,16 +30,26 @@ export default {
     data() {
         return {
             loadingPokedex: true,
+            loadingFilters: true,
+            types: null,
             pokedex: null
         }
     },
     async created() {
         try {
+            const res = await fetch('/api/taxonomies/types/terms');
+            const { data } = await res.json();
+            this.types = data;
+            this.loadingFilters = false;
+            console.log(this.types);
+        } catch(e) {
+            console.log(e);
+        }
+        try {
             const res = await fetch('/api/collections/pokemon/entries?limit=2000');
             const { data } = await res.json();
             this.pokedex = data;
             this.loadingPokedex = false;
-            console.log(this.pokedex);
         } catch(e) {
             console.log(e);
         }
