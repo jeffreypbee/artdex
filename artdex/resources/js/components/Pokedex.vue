b27a3c2c620d50fbf0d0533311a581f47fefa858
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-02 17:55:15 -0400

    Removed API integration

--- artdex/resources/js/components/Pokedex.vue
+++ artdex/resources/js/components/Pokedex.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="bg-gray">
-        <div v-if="loadingFilters">Loading</div>
-        <div v-else class="flex">
+        <div class="flex">
             <div v-for="type in types" :key="type.id">
                 <i class="fa-solid p-1" :class="type.icon" :style="{ color: type.color }"></i>
             </div>
         </div>
+
     </div>
     <div>
         <div v-if="loadingPokedex">
@@ -27,32 +27,33 @@ export default {
     components: {
         PokemonCard
     },
+    props: ['pokedex', 'types', 'tags'],
     data() {
         return {
-            loadingPokedex: true,
-            loadingFilters: true,
-            types: null,
-            pokedex: null
+            // loadingPokedex: true,
+            // loadingFilters: true,
+            // types: null,
+            // pokedex: null
         }
     },
-    async created() {
-        try {
-            const res = await fetch('/api/taxonomies/types/terms');
-            const { data } = await res.json();
-            this.types = data;
-            this.loadingFilters = false;
-        } catch(e) {
-            console.log(e);
-        }
-        try {
-            const res = await fetch('/api/collections/pokemon/entries?limit=2000');
-            const { data } = await res.json();
-            this.pokedex = data;
-            this.loadingPokedex = false;
-        } catch(e) {
-            console.log(e);
-        }
-    }
+    // async created() {
+    //     try {
+    //         const res = await fetch('/api/taxonomies/types/terms');
+    //         const { data } = await res.json();
+    //         this.types = data;
+    //         this.loadingFilters = false;
+    //     } catch(e) {
+    //         console.log(e);
+    //     }
+    //     try {
+    //         const res = await fetch('/api/collections/pokemon/entries?limit=2000');
+    //         const { data } = await res.json();
+    //         this.pokedex = data;
+    //         this.loadingPokedex = false;
+    //     } catch(e) {
+    //         console.log(e);
+    //     }
+    // }
 }
 </script>
 
--- artdex/resources/js/components/PokemonCard.vue
+++ artdex/resources/js/components/PokemonCard.vue
@@ -10,8 +10,8 @@
             <div class="h-3/4 flex flex-col items-end justify-around pb-1 pr-2 leading-none">
                 <div><span class="opacity-50"># </span>{{ pokemon.formatted_number }}</div>
                 <div>{{ pokemon.title }}</div>
-                <div v-if="!loadingTypes">
-                    <!-- <i v-for="type in pokemon.types" :key="pokemon.id + '-' + type.id" :class="'fa-solid' + type-icon + 'p-1'" :style="{ color: type.color }"></i> -->
+                <div>
+                    <i v-for="type in pokemon.types" :key="pokemon.id + '-' + type.id" :class="'fa-solid' + type-icon + 'p-1'" :style="{ color: type.color }"></i>
                 </div>
             </div>
         </div>
@@ -23,25 +23,25 @@ export default {
     props: ['pokemon'],
     data() {
         return {
-            types: [],
-            loadingTypes: true
+
         }
     },
-    async created() {
-        let filterArray = [];
-        this.pokemon.types.forEach(element => {
-            filterArray.push(element.slug);
-        });
-        const filters = filterArray.join('|');
+    // async created() {
+    //     let filterArray = [];
+    //     this.pokemon.types.forEach(element => {
+    //         filterArray.push(element.slug);
+    //     });
+    //     const filters = filterArray.join('|');
 
-        try {
-            const res = await fetch('/api/taxonomies/types/terms?filter[slug]=' + filters);
-            const { data } = await res.json();
-            this.types = data;
-        } catch(e) {
-            console.log(e);
-        }
-    }
+    //     try {
+    //         const res = await fetch('/api/taxonomies/types/terms?filter[slug]=' + filters);
+    //         const { data } = await res.json();
+    //         this.types = data;
+    //         console.log(this.types);
+    //     } catch(e) {
+    //         console.log(e);
+    //     }
+    // }
 }
 </script>
 
