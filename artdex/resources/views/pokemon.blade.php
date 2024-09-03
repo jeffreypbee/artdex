b988a1ccf906ff56649e9797ece5d386fc1c1b0f
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-03 16:41:00 -0400

    Redesigning pokemon show page

--- artdex/resources/views/pokemon.blade.php
+++ artdex/resources/views/pokemon.blade.php
@@ -17,17 +17,45 @@
             </a>
         @endunless
     </div>
+
+    @php
+        $background = 'linear-gradient(to right';
+        foreach ($pokemon->getColors() as $color) {
+            $background .= ', ' . $color;
+        }
+    @endphp
+
     <div class="flex flex-col items-center">
+        <div class="h-96 w-2/5 rounded-2xl relative shadow-md shadow-black"
+            style="background: rgb(40, 52, 90)">
+            <div class="flex justify-end px-4 items-center h-1/6 rounded-t-2xl border-b-2 border-white text-darkgray font-bold text-2xl"
+                style="background: {{$background}}">
+                {{$pokemon->name}}
+            </div>
+            <div>
+                @if ($pokemon->hasArt())
+                    <img class="w-64 lg:absolute lg:w-auto z-10 -bottom-16 -left-5 transition-all"
+                    src="/storage/{{$pokemon->art[0]->image}}" alt="">
+                @endif
+            </div>
+            <div class="h-5/6 flex flex-col items-end justify-between leading-none text-lg">
+                <div class="w-full h-full p-4 flex flex-col items-end justify-evenly leading-none rounded-b-2xl"
+                    style="background: rgb(40, 52, 90)">
+                    <div><span class="opacity-50"># </span>{{$pokemon->getNumber()}}</div>
 
-        <x-generation-pill :generation="$pokemon->generation" />
+                    <x-generation-pill :generation="$pokemon->generation" />
 
-        <div class="flex m-2 gap-2">
-            <x-type-pill :type="$pokemon->type1" />
-            @unless ($pokemon->type2 == null)
-            <x-type-pill :type="$pokemon->type2" />
-            @endunless
+                    <div class="flex m-2 gap-2">
+                        <x-type-pill :type="$pokemon->type1" />
+                        @unless ($pokemon->type2 == null)
+                        <x-type-pill :type="$pokemon->type2" />
+                        @endunless
+                    </div>
+                </div>
+            </div>
         </div>
 
+        Art
         @foreach ($pokemon->art as $art)
             @if ($art->isPublished())
                 <img src="/storage/{{$art->image}}" alt="">
