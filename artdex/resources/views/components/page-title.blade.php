315cf8b5ac6c8005bfe0c1ee50941c21a0ff499f
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-15 08:54:56 -0400

    Update page-title.blade.php

--- artdex/resources/views/components/page-title.blade.php
+++ artdex/resources/views/components/page-title.blade.php
@@ -1,4 +1,5 @@
-<div class="w-full text-2xl flex justify-center mb-5 border-b-2 border-gray-400"
+<div class="w-full text-2xl flex flex-col items-center mb-5"
     style="background: linear-gradient(to right, transparent, rgb(40, 52, 90), transparent)">
     {{ $slot }}
+    <div class="w-full h-1 bg-gradient-to-r from-transparent via-white"></div>
 </div>
