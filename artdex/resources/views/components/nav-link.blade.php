fd016cb489cce2951ba6f0fdf6e799df50a81598
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-22 16:00:36 -0400

    Update nav-link.blade.php

--- artdex/resources/views/components/nav-link.blade.php
+++ artdex/resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'p-2 flex justify-between items-end leading-none gap-1 border-r-2 border-white text-sm font-medium focus:outline-none focus:border-white transition duration-150 ease-in-out'
+            ? 'p-2 flex justify-between items-end leading-none gap-1 border-r-2 bg-blue-gray text-sm font-medium focus:outline-none focus:border-white transition duration-150 ease-in-out'
             : 'p-2 flex justify-between items-end leading-none gap-1 border-r-2 border-transparent text-sm font-medium hover:border-gray-400 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
