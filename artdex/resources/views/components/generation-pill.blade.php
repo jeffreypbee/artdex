d4d9e4b8d34138097c9f247abceebfce201202d5
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-22 15:45:17 -0400

    Update generation-pill.blade.php

--- artdex/resources/views/components/generation-pill.blade.php
+++ artdex/resources/views/components/generation-pill.blade.php
@@ -1,12 +1,12 @@
 @props(['generation'])
 
 <a href="/dex?gen={{$generation->generation}}">
-    <div class="flex justify-between items-center p-1 rounded-lg font-extrabold bg-blue-gray"
-        style="width: 150px; background: linear-gradient(-125deg, {{$generation->color}} 38%, rgb(40, 52, 90) 39%)">
-        <div class="pl-1">Generation</div>
-        <div class="p-1 leading-none rounded-lg font-extrabold text-xl bg-blue-gray" style="color: {{$generation->color}}">
+    <div class="w-28 h-8 flex justify-between items-center p-1 rounded-lg font-extrabold bg-blue-gray hover:scale-105 transition-all shadow-md shadow-black"
+        style="background: linear-gradient(-125deg, {{$generation->color}} 38%, rgb(40, 52, 90) 39%)">
+        <div class="pl-1">Gen</div>
+        <div class="p-1 leading-none rounded-lg font-extrabold text-xl">
             {{-- {{$generation->generation}} --}}
-            <i class="fa-solid {{$generation->icon}} fa-fw fa-lg" style="color: {{$generation->color}}"></i>
+            <i class="fa-solid {{$generation->icon}} fa-fw fa-lg text-slate-900"></i>
         </div>
     </div>
 </a>
