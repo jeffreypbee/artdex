88ceaa349780b94eef03632b6082d7b0c3574873
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-22 15:31:09 -0400

    Update type-pill.blade.php

--- artdex/resources/views/components/type-pill.blade.php
+++ artdex/resources/views/components/type-pill.blade.php
@@ -1,9 +1,9 @@
 @props(['type'])
 
 <a href="/dex?type={{$type->name}}">
-    <div class="flex justify-between items-center p-1 rounded-lg font-extrabold"
-        style="width: 100px; background: linear-gradient(125deg, {{$type->color}} 35%, rgb(40, 52, 90) 36%)">
-        <i class="fa-solid {{$type->icon}} fa-fw"></i>
+    <div class="w-28 h-8 flex justify-between items-center p-1 rounded-lg font-extrabold shadow-md shadow-black hover:scale-105 transition-all"
+        style="background: linear-gradient(125deg, {{$type->color}} 35%, rgb(40, 52, 90) 36%)">
+        <i class="fa-solid {{$type->icon}} fa-fw text-slate-900"></i>
         <div class="pr-1">{{$type->name}}</div>
     </div>
 </a>
