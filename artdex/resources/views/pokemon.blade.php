fc50b01e7bfecdeda59cc29706ebe1794d033a2e
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-24 19:31:53 -0400

    Redesigning pokemon page

--- artdex/resources/views/pokemon.blade.php
+++ artdex/resources/views/pokemon.blade.php
@@ -2,26 +2,30 @@
     <x-page-title>#{{$pokemon->getNumber()}} - {{$pokemon->name}}</x-page-title>
 
     <div class="my-4 w-full flex justify-center gap-4">
-        <a href="#forms">Forms</a>
-        <a href="#gallery">Gallery</a>
+        <a href="#forms"><button class="p-2 rounded-xl hover:bg-blue-gray">Forms</button></a>
+        <a href="#gallery"><button class="p-2 rounded-xl hover:bg-blue-gray">Gallery</button></a>
     </div>
 
-    <div class="w-full relative">
+    <div class="w-full grid grid-cols-2">
         @php
             $prev = $pokemon->previous();
             $next = $pokemon->next();
         @endphp
-        @unless ($prev == null)
+        <div class="flex justify-start">
+            @unless ($prev == null)
             <a href="/pokemon/{{$prev->name}}">
-                <button class="absolute left-10 p-2 rounded-xl hover:bg-blue-gray">< #{{$prev->getNumber()}} - {{$prev->name}}</button>
+                <button class="p-2 rounded-xl hover:bg-blue-gray">< #{{$prev->getNumber()}} - {{$prev->name}}</button>
             </a>
-        @endunless
+            @endunless
+        </div>
 
-        @unless ($next == null)
+        <div class="flex justify-end">
+            @unless ($next == null)
             <a href="/pokemon/{{$next->name}}">
-                <button class="absolute right-10 p-2 rounded-xl hover:bg-blue-gray">#{{$next->getNumber()}} - {{$next->name}} ></button>
+                <button class="p-2 rounded-xl hover:bg-blue-gray">#{{$next->getNumber()}} - {{$next->name}} ></button>
             </a>
-        @endunless
+            @endunless
+        </div>
     </div>
 
     @php
@@ -38,26 +42,20 @@
     @endphp
 
     <div class="flex flex-col items-center">
-        <div class="my-4 h-96 w-2/5 rounded-2xl relative shadow-md shadow-black"
-            style="background: rgb(40, 52, 90)">
-            <div class="flex justify-end px-4 items-center h-1/6 rounded-t-2xl border-b-2 border-white text-slate-900 font-bold text-2xl"
-                style="background: {{$background}}">
-                {{$pokemon->name}}
-            </div>
+
+        <section class="md:flex">
             <div>
-                <img src="/images/pokeball_gray.png" alt="" class="w-72 absolute left-30 bottom-5 opacity-50 blur-md">
                 @if ($pokemon->hasArt())
-                    <img class="w-64 lg:absolute lg:w-auto z-10 -top-10 -left-24 transition-all"
-                    src="/storage/{{$pokemon->art[0]->image}}" alt="">
+                <img src="/storage/{{$pokemon->art[0]->image}}" alt="">
                 @endif
             </div>
-            <div class="h-5/6 flex flex-col items-end justify-between leading-none text-lg">
-                <div class="relative z-20 w-full h-full p-4 flex flex-col items-end justify-end gap-4 leading-none rounded-b-2xl"
-                    >
-                    <div><span class="opacity-50"># </span>{{$pokemon->getNumber()}}</div>
-
+            <div class="w-96 rounded-t-2xl bg-gradient-to-b from-blue-gray">
+                <div class="w-full flex justify-end px-4 rounded-t-2xl border-b-2 border-white text-2xl" style="background: {{$background}}">
+                    <span class="text-blue-gray">{{$pokemon->getNumber()}}</span>&nbsp;
+                    <span class="text-slate-900 font-bold">{{$pokemon->name}}</span>
+                </div>
+                <div class="mt-10 flex flex-col gap-4 items-center">
                     <x-generation-pill :generation="$pokemon->generation" />
-
                     <div class="flex m-2 gap-2">
                         <x-type-pill :type="$pokemon->type1" />
                         @unless ($pokemon->type2 == null)
@@ -66,7 +64,8 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
 
         <section class="w-full m-10">
             <a id="forms"></a>
