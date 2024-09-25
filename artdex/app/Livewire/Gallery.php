a9f5685c5d05262f233ee13abde92dbbddf1e2d6
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-25 13:32:21 -0400

    Added sort to gallery

--- artdex/app/Livewire/Gallery.php
+++ artdex/app/Livewire/Gallery.php
@@ -4,13 +4,39 @@ namespace App\Livewire;
 
 use App\Models\Art;
 use Livewire\Component;
+use Livewire\Attributes\Url;
 
 class Gallery extends Component
 {
+
+    public $gallery;
+
+    #[Url]
+    public $sort = '';
+
+    public function mount() {
+        $this->sortGallery();
+    }
+
+    public function sortBy($sortBy) {
+        $this->sort = $sortBy;
+        $this->sortGallery();
+    }
+
+    public function sortGallery() {
+        if ($this->sort === 'new') {
+            $this->gallery = Art::published()->orderBy('publish_date', 'desc')->get();
+        } elseif ($this->sort === 'old') {
+            $this->gallery = Art::published()->orderBy('publish_date', 'asc')->get();
+        } else {
+            $this->gallery = Art::published()->latest('publish_date')->get();
+        }
+    }
+
     public function render()
     {
         return view('livewire.gallery', [
-            'gallery' => Art::published()->latest('publish_date')->get()
+            'gallery' => $this->gallery
         ]);
     }
 }
--- artdex/resources/views/livewire/gallery.blade.php
+++ artdex/resources/views/livewire/gallery.blade.php
@@ -1,5 +1,14 @@
-<div class="flex flex-wrap justify-center">
-    @foreach ($gallery as $art)
-        <livewire:gallery-entry :art="$art" />
-    @endforeach
+<div>
+    <div class="mb-4 flex justify-center gap-2">
+        <button class="p-2 rounded hover:bg-blue-gray {{$this->sort === 'new' ? 'bg-white text-slate-900' : ''}}"
+            wire:click='sortBy("new")'>Newest</button>
+        <button class="p-2 rounded hover:bg-blue-gray {{$this->sort === 'old' ? 'bg-white text-slate-900' : ''}}"
+            wire:click='sortBy("old")'>Oldest</button>
+    </div>
+
+    <div class="flex flex-wrap justify-center">
+        @foreach ($gallery as $art)
+            <livewire:gallery-entry wire:key='{{$art->id}}' :art="$art" />
+        @endforeach
+    </div>
 </div>
