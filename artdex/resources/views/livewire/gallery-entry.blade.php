1aade10165b38893d94f02a693aba08051a9887d
Jeffrey Bee <jeffreypbee@gmail.com>
2024-09-25 12:28:57 -0400

    Update gallery-entry.blade.php

--- artdex/resources/views/livewire/gallery-entry.blade.php
+++ artdex/resources/views/livewire/gallery-entry.blade.php
@@ -2,18 +2,16 @@
 <div x-data="{showInfo: false}"
     @mouseenter="showInfo = true"
     @mouseleave="showInfo = false"
-    class="relative hover:cursor-pointer hover:bg-blue-gray rounded-lg">
-    <img src="/storage/{{$art->image}}" alt="" class="" style="width: 250px">
+    class="relative hover:cursor-pointer hover:bg-blue-gray">
+    <img src="/storage/{{$art->image}}" alt="" class="">
     <div
         x-show="showInfo"
-        class="p-2 flex flex-col absolute bottom-0 left-0 w-full backdrop-blur-sm rounded-lg">
-        <div>
-            #{{$number}}
+        class="w-full h-full p-2 flex flex-col justify-between items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div class="w-full text-2xl backdrop-blur-sm">
+            <span>{{$number}}</span>&nbsp;
+            <span class="font-bold">{{$title}}</span>
         </div>
-        <div>
-            {{$title}}
-        </div>
-        <div>
+        <div class="w-full flex justify-end backdrop-blur-sm">
             {{date('M d, Y', strtotime($art->publish_date))}}
         </div>
     </div>
