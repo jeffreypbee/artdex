ad487af7392a9c152fa1a0978090fa91f8eb6dd1
Jeffrey Bee <jeffreypbee@gmail.com>
2025-07-27 08:42:18 -0400

    Update 2025_06_14_215445_create_tags_table.php

--- artdex/database/migrations/2025_06_14_215445_create_tags_table.php
+++ artdex/database/migrations/2025_06_14_215445_create_tags_table.php
@@ -14,6 +14,17 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('icon');
+            $table->string('color');
+        });
+
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
+            $table->morphs('taggable');
         });
     }
 
@@ -22,6 +33,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('taggables');
         Schema::dropIfExists('tags');
     }
 };
